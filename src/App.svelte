f77b26dd59c21462ba850e209f8ce6a0ae8530ae
Ar1ad <sora4gamers@gmail.com>
2021-03-09 14:35:18 +0100

    timed functions and more upgrades
    I've added timed functions so that there's a framework for getting coins/s and added more test upgrades

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	export let name: string;
 	let coin = 0;
 	let clickpower = 1;
+	let CoinPS = 0;
+	
+	var timer = setInterval(_GiveCPS, 1000)
 
 	let upgrades1 = 0
+	let upgrades2 = 0
 
 	let Cost_upgrade1 = 10;
+	let Cost_upgrade2 = 50;
+
+	let psupgrades1 = 0
+
+	let Cost_psupgrade1 = 100;
+
+	
+
+	function _GiveCPS(){
+		coin += CoinPS
+	}
+
 	function _CoinClicked(){
 		coin += clickpower
 	}
@@ -19,7 +34,23 @@
 			clickpower += 1
 			upgrades1 += 1
 			coin -= Cost_upgrade1
-			Cost_upgrade1 = 10 * Math.round(Math.pow((upgrades1 * 1.5), 2)) + 10
+			Cost_upgrade1 = Math.round(Math.pow((upgrades1), 2) * upgrades1) + 10
+		}
+	}
+	function _Upgrade2(){
+		if(_Buyable(Cost_upgrade2)){
+			clickpower += 2
+			upgrades2 += 1
+			coin -= Cost_upgrade2
+			Cost_upgrade2 = 10 * Math.round(Math.pow((upgrades2 * 2), 2) * upgrades2) + 50
+		}
+	}
+	function _PsUpgrade1(){
+		if(_Buyable(Cost_psupgrade1)){
+			CoinPS += 1
+			psupgrades1 += 1
+			coin -= Cost_psupgrade1
+			Cost_psupgrade1 = 10 * Math.round(Math.pow((psupgrades1 * 2), 2) * psupgrades1) + 100
 		}
 	}
 </script>
@@ -28,42 +59,86 @@
 	<title>Svelte-Clicker</title>
 </svelte:head>
 
+
 <main>
-	<div>
-		<h1>Hello {name}!</h1>
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	</div>
-	<div>
-		<p>ClickPower: {clickpower}</p>
-		<button on:click="{_CoinClicked}">Get Coin</button>
-	</div>
-	<div>
+	<div class="grid-container-Main">
+		<div>
+			<p>ClickPower: {clickpower}</p>
+			<button on:click="{_CoinClicked}">Get Coin</button>
+		</div>
 		<p>Coins = {coin}</p>
 	</div>
-	<div>
-		<button on:click="{_Upgrade1}">+1</button>
-		<p>costs: {Cost_upgrade1} coins</p>
+	
+	<div class="grid-container-Upgrades">
+		<div class="upgradeButtons">
+			<button on:click="{_Upgrade1}">+1</button>
+			<p class="upgradeCost">costs: {Cost_upgrade1} coins</p>
+		</div>
+		<div class="upgradeButtons">
+			<button on:click="{_Upgrade2}">+2</button>
+			<p class="upgradeCost">costs: {Cost_upgrade2} coins</p>
+		</div>
+		<div class="upgradeButtons">
+			<button on:click="{_PsUpgrade1}">+2</button>
+			<p class="upgradeCost">costs: {Cost_psupgrade1} coins</p>
+		</div>
 	</div>
+	
+	<footer>
+		<p>built using <a href="https://svelte.dev">Svelte</a> and <a href="https://www.typescriptlang.org/">typescript</a></p>
+	</footer>
 </main>
+
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		height: 90%;
+		display: grid;
+		grid-template-columns: [ClickAreaStart]3fr [ClickAreaEnd UpgradeAreaStart] 1fr [UpgradeAreaEnd];
+		grid-template-rows: [GameAreaStart] 9fr [GameAreaEnd FooterStart] 1fr [FooterEnd] ;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	p {
+		margin: inherit;
 	}
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+	}
+
+	.grid-container-Main{
+		grid-column-start: ClickAreaStart;
+		grid-column-end: ClickAreaEnd;
+		grid-row-start: GameAreaStart;
+		grid-row-end: GameAreaEnd;
+	}
+
+	.grid-container-Upgrades{
+		grid-column-start: UpgradeAreaStart;
+		grid-column-end: UpgradeAreaEnd;
+		grid-row-start: GameAreaStart;
+		grid-row-end: GameAreaEnd;
+		
+	}
+
+	.upgradeCost{
+		margin: 0px;
+	}
+
+	.upgradeButtons {
+		margin: 5px;
+		background-color: beige;
+	}
+
+	footer{
+		grid-row-start: FooterStart;
+		grid-row-end: FooterEnd;
+		background-color: darkslategray;
+		color:floralwhite;
 	}
 </style>
